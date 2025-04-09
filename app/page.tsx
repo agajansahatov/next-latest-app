7bab41dbafc75b37954cbba055dda51359752034
Agajan Sahatov <agajan.st@gmail.com>
2025-04-09 21:16:13 +0800

    Basic Routing and Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-	return <h1>Hello World!</h1>;
+	return (
+		<>
+			<h1>Hello World!</h1>
+			<ul>
+				<li>
+					<a href="/users">Users - Server side navigation</a>
+				</li>
+				<li>
+					<Link href="/users">Users - Client side navigation</Link>
+				</li>
+			</ul>
+		</>
+	);
 }
