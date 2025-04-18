85e82193c65f3fb15cc038db68d638fe94270bbf
Agajan Sahatov <agajan.st@gmail.com>
2025-04-18 19:43:33 +0800

    Roouting Overview

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,45 +1,11 @@
 import React from "react";
-
-interface User {
-	id: number;
-	name: string;
-	email: string;
-}
+import UserTable from "./UserTable";
 
 const UsersPage = async () => {
-	const res = await fetch(
-		"https://jsonplaceholder.typicode.com/users",
-		// By default it caches
-		{ cache: "no-store" },
-		// { next: { revalidate: 10 } },
-	);
-	// This cache functionality is only implemented in the fetch function, not in other 3rd party libraries like axios
-
-	const users: User[] = await res.json();
-
-	// By default the server component does static rendering on production mode,
-	// But if we change caching to "no-store" or revalidate for a period of time,
-	// it does dynamic rendering
-
 	return (
 		<>
 			<h1 className="text-center text-3xl p-5">Users</h1>
-			<table className="table table-bordered p-5">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Email</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map((user) => (
-						<tr key={user.id}>
-							<td>{user.name}</td>
-							<td>{user.email}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+			<UserTable />
 		</>
 	);
 };
