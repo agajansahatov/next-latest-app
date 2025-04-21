7eb3936e46486dc879473c2114c6014f2f420fdc
Agajan Sahatov <agajan.st@gmail.com>
2025-04-21 17:09:42 +0800

    Building APIs: Getting a single object

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+	request: NextRequest,
+	{ params }: { params: Promise<{ id: number }> },
+) {
+	const { id } = await params;
+	if (id > 10)
+		return NextResponse.json({ error: "User not found" }, { status: 404 });
+	return NextResponse.json({ id: 1, name: "Agajan" });
+}
