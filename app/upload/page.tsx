e2226e39751e0c3fbc10637d345633af9f0a5523
Agajan Sahatov <agajan.st@gmail.com>
2025-04-24 22:32:40 +0800

    Showing Uploaded Images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,41 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+	public_id: string;
+}
 
 const UploadPage = () => {
+	const [publicId, setPublicId] = useState("");
+
 	return (
-		<CldUploadWidget uploadPreset="next-latest-app">
-			{({ open }) => (
-				<button className="btn btn-primary" onClick={() => open()}>
-					Upload
-				</button>
+		<>
+			{publicId && (
+				<CldImage
+					src={publicId}
+					width={270}
+					height={180}
+					alt="A coffee image"
+					className="pb-5"
+				/>
 			)}
-		</CldUploadWidget>
+			<CldUploadWidget
+				uploadPreset="next-latest-app"
+				onSuccess={(result) => {
+					if (result.event !== "success") return;
+					const info = result.info as CloudinaryResult;
+					console.log(info.public_id);
+					setPublicId(info.public_id);
+				}}
+			>
+				{({ open }) => (
+					<button className="btn btn-primary" onClick={() => open()}>
+						Upload
+					</button>
+				)}
+			</CldUploadWidget>
+		</>
 	);
 };
 
